788134c6fd37269c999d8c5eca29d0451707a644
ion232 <66679118+ion232@users.noreply.github.com>
2023-03-03 10:47:15 +0000

    Add missing test fixtures files and config

---
+++ test/fixtures/zig/build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const dependency_options = .{ .target = target, .optimize = optimize };
+    const json_module = b.dependency("json", dependency_options).module("json");
+
+    const exe = b.addExecutable(.{
+        .name = "zig",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.addModule("json", json_module);
+    exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.condition = .always;
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ test/fixtures/zig/src/main.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+
+const json = @import("json");
+const quicktype = @import("quicktype.zig");
+
+pub fn main() !void {
+    var args = std.process.args();
+    const file_name = blk: {
+        _ = args.next(); // Skip program name.
+        break :blk args.next() orelse unreachable;
+    };
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
+
+    var json_file = try std.fs.cwd().openFile(file_name, .{});
+    defer json_file.close();
+    const size = (try json_file.stat()).size;
+
+    var json_buffer: []u8 = allocator.alloc(u8, size) catch unreachable;
+    defer allocator.free(json_buffer);
+    const bytes_read = try json_file.read(json_buffer);
+
+    if (bytes_read != size) {
+        std.debug.print("Failed to read all bytes.", .{});
+    }
+
+    try test_model(allocator, json_buffer);
+}
+
+fn test_model(allocator: std.mem.Allocator, json_buffer: []const u8) !void {
+    const model = json.fromSlice(allocator, quicktype.TestFile, json_buffer) catch |e| {
+        return std.debug.print("Failed to deserialize: {any}.", .{e});
+    };
+    const string = json.toSlice(allocator, model) catch |e| {
+        return std.debug.print("Failed to serialize: {any}", .{e});
+    };
+    std.io.getStdOut().writer().writeAll(string) catch |e| {
+        return std.debug.print("Failed to write to stdout: {any}", .{e});
+    };
+}
